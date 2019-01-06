ab7014afc6c437558917138690988afd4e113ba4
jaysonesmith <github@nes.33mail.com>
2019-01-06 00:38:58 -0700

    Y can bespecified and works + refactor to defaultDim

--- board/board.go
+++ board/board.go
@@ -6,15 +6,22 @@ type Board [][]string
 // an input dimension number. The board size
 // returned defaults to 4 for anything under that
 // size.
-func New(x int) Board {
-	if x < 4 {
-		x = 4
-	}
+func New(x, y int) Board {
+	x = defaultDim(x)
+	y = defaultDim(y)
 
 	b := make([][]string, x)
 	for i := range b {
-		b[i] = make([]string, x)
+		b[i] = make([]string, y)
 	}
 
 	return b
+}
+
+func defaultDim(i int) int {
+	if i < 4 {
+		return 4
+	}
+
+	return i
 }
--- board/board_test.go
+++ board/board_test.go
@@ -10,21 +10,23 @@ func TestNew(t *testing.T) {
 	testCases := []struct {
 		name string
 		x    int
+		y    int
 		xl   int
 		yl   int
 	}{
-		{name: "0 defaults to 4x4", x: 0, xl: 4, yl: 4},
-		{name: "1 defaults to 4x4", x: 1, xl: 4, yl: 4},
-		{name: "2 defaults to 4x4", x: 2, xl: 4, yl: 4},
-		{name: "3 defaults to 4x4", x: 3, xl: 4, yl: 4},
-		{name: "4 returns 4x4", x: 4, xl: 4, yl: 4},
-		{name: "10 returns 10x10", x: 10, xl: 10, yl: 10},
-		{name: "1000 returns 10x10", x: 1000, xl: 1000, yl: 1000},
+		{name: "0 defaults to 4x4", x: 0, y: 0, xl: 4, yl: 4},
+		{name: "1 defaults to 4x4", x: 1, y: 1, xl: 4, yl: 4},
+		{name: "2 defaults to 4x4", x: 2, y: 2, xl: 4, yl: 4},
+		{name: "3 defaults to 4x4", x: 3, y: 3, xl: 4, yl: 4},
+		{name: "4 returns 4x4", x: 4, y: 4, xl: 4, yl: 4},
+		{name: "10 returns 10x10", x: 10, y: 10, xl: 10, yl: 10},
+		{name: "1000 returns 10x10", x: 1000,
+			y: 1000, xl: 1000, yl: 1000},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(tt *testing.T) {
-			actual := New(tc.x)
+			actual := New(tc.x, tc.y)
 
 			assert.Equal(tt, tc.xl, len(actual))
 			assert.Equal(tt, tc.yl, len(actual[0]))
